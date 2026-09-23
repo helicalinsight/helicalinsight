ebac4130447f389e24ee7c43ce224b63a176f0ac
Ummadi Hemalatha <22x51a05e9@srecnandyal.edu.in>
2026-09-23 21:43:01 +0530

    Add MongoDB JDBC driver support

--- server/core/src/main/java/com/helicalinsight/datasource/MongoConnectionFactory.java
+++ server/core/src/main/java/com/helicalinsight/datasource/MongoConnectionFactory.java
@@ -67,10 +67,10 @@ public class MongoConnectionFactory extends DatabaseConnectionFactory {
 				driverClassName = connectionDetails.get("driverName").getAsString();
 			}
 
-			if ("mongodb.jdbc.MongoDriver".equalsIgnoreCase(driverClassName)) {
+			if ("com.mongodb.jdbc.MongoDriver".equalsIgnoreCase(driverClassName)) {
 				DriverConnection driverConnection = new DriverConnection();
 				driverConnection.setConnection(null);
-				driverConnection.setDriverClass("mongodb.jdbc.MongoDriver");
+				driverConnection.setDriverClass("com.mongodb.jdbc.MongoDriver");
 				return driverConnection;
 
 			}
